a4d2449a8686516fa8b5197690bb0b70b5e9f357
rkq00 <60001560+rk-q09@users.noreply.github.com>
2022-02-09 22:24:56 +0900

    test: user model

---
+++ src/models/__tests__/user.ts
@@ -0,0 +1,23 @@
+import { User } from '@models/user';
+import { faker } from '@faker-js/faker';
+
+describe('save', () => {
+  it('should create user', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const username = faker.internet.userName();
+
+    const user = User.build({ username, email, password });
+    await user.save();
+
+    const fetched = await User.findOne({ email });
+
+    if (!fetched) { throw new Error("User is null"); } 
+
+    expect(fetched.email).toBe(email);
+    expect(fetched.username).toBe(username);
+    expect(fetched.password).not.toBe(password);
+  });
+});
+
+
