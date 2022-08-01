92a0da13f8b94a98ce8ed5fa8fdbdfea0e73e347
rkq00 <60001560+rk-q09@users.noreply.github.com>
2022-08-01 18:48:28 +0900

    feat: apply cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import passport from '@config/passport';
+import cors from 'cors';
 
 import { router } from '@routes';
 import { NotFoundError } from '@errors/not-found-error';
@@ -11,6 +12,7 @@ const app = express();
 /**
  * ----- Global Middleware -----
  */
+app.use(cors());
 app.use(express.json());
 app.set('trust proxy', true);
 
